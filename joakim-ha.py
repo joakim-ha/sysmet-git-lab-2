2ffa480f4c5ef1494ad841ad271d7253586ab1b9
joakim-ha <68053006+joakim-ha@users.noreply.github.com>
2025-12-12 15:57:04 +0200

    Create joakim-ha.c

---
+++ joakim-ha.py
@@ -0,0 +1,4 @@
+print("Git commit messages should be reasonably short, but descriptive. See also:")
+print("https://www.reddit.com/r/ProgrammerHumor/comments/ntt4w8/github_commit_message_of_the_year/")
+print("Or for a more scientific approach to the subject:")
+print("https://geeksta.net/geeklog/exploring-expressions-emotions-github-commit-messages/")
